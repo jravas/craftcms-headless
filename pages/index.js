a305b00adc431ebf70166f7ca69976090f7636a2
jravas <josip@prototyp.digital>
2023-11-21 19:30:03 +0100

    Fixed docker node

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:8080/api?query=%7Bping%7D");
+  const quotes = await res.json();
+
+  return {
+    props: {
+      quotes: quotes,
+    },
+  };
+}
+
+export default function Home({ quotes }) {
+  console.log(quotes)
   return (
     <div className={styles.container}>
       <Head>
@@ -11,45 +23,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
+          {/* <span>Total quotes: {quotes}</span> */}
           Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
